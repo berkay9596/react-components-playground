8139de1d58638ae81c02ce35dcebd3f92b16b2c2
Mustafaberat <mustafa.ceren.18@hotmail.com>
2019-06-17 17:27:44 +0300

    Adone 6 Dark Alert

--- src/components/Addon6.js
+++ src/components/Addon6.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
-
+import Alert from 'react-bootstrap/Alert'
+const scope = {Alert};
 class Addon6 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon6</div>" ></FiddleComponent>
+                <FiddleComponent code="<Alert variant = 'dark'>This is a mustafaberat alert with{' '}</Alert>" scope = {scope}></FiddleComponent>
+
             </div>
         );
     }
