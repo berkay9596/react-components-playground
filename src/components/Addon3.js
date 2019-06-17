b2bdc5710fbdf789075a6b5e67bba54b23e10aa8
begum <begumozbayy@gmail.com>
2019-06-17 17:22:41 +0300

    sixth commit

--- src/components/Addon3.js
+++ src/components/Addon3.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Alert from 'react-bootstrap/Alert';
 
 class Addon3 extends Component {
     render() {
+
         return (
             <div>
                 <FiddleComponent code="<div>this is Addon3</div>" ></FiddleComponent>
+                 <Alert variant="secondary">
+                    This is a secondary alert—check it out!
+                  </Alert>
             </div>
         );
     }
