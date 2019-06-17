ad8654986fb8f64ef7dcc60ca35746b0cfa4a78d
noxlumos <97zeyneperdogan@gmail.com>
2019-06-17 17:22:03 +0300

    Adon2 Dropdown Zeynep

--- src/components/Addon2.js
+++ src/components/Addon2.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const scope = {Dropdown};
 
 class Addon2 extends Component {
     render() {
         return (
-            <div>
-                <FiddleComponent code="<div>this is Addon2</div>" ></FiddleComponent>
-            </div>
+
+
+
+          <div>
+            <FiddleComponent code='<div>  <Dropdown>
+                                                                             <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                                                               Dropdown Button
+                                                                             </Dropdown.Toggle>
+                                                                             <Dropdown.Menu>
+                                                                               <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                                                               <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                                                               <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                                                             </Dropdown.Menu>
+                                                                           </Dropdown></div>' scope={scope} ></FiddleComponent>
+
+          </div>
+
         );
     }
 }
