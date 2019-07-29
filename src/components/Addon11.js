184ed43c2f4b97115e0e5a3b736f5df4643efd03
Sefa Berat Canoğlu <38809766+crotsi@users.noreply.github.com>
2019-07-29 16:28:31 +0300

    sefa
    sefa

--- src/components/Addon11.js
+++ src/components/Addon11.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Alert from 'react-bootstrap/Alert';
 
+
+const scope = {Alert};
 class Addon11 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon11 sefa</div>" ></FiddleComponent>
+
+                <FiddleComponent code="
+                [
+  'primary',
+  'secondary',
+  'success',
+  'danger',
+  'warning',
+  'info',
+  'light',
+  'dark',
+].map((variant, idx) => (
+  <Alert key={idx} variant={variant}>
+    This is a {variant} alert—check it out!
+  </Alert>
+));" scope={scope} ></FiddleComponent>
             </div>
         );
     }
