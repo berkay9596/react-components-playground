72166f47157ff9d92e39dccbd5cde641827a2030
Meltem Tokgöz <meltemtgz@gmail.com>
2019-07-31 17:06:06 +0300

    Update Addon37.js

--- src/components/Addon37.js
+++ src/components/Addon37.js
@@ -5,7 +5,8 @@ class Addon17 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon37</div>" ></FiddleComponent>
+                meltem 
             </div>
         );
     }
