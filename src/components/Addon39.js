cfe0c8302a2155dcc60385b048e2c5362f8fdb00
ahmetemrekilinc <aekilinc@gmail.com>
2019-07-31 17:15:28 +0300

    39

--- src/components/Addon39.js
+++ src/components/Addon39.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Badge from 'react-bootstrap/Badge';
+
+
+const scope = {Badge};
 
 class Addon39 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon39</div>" ></FiddleComponent>
+                <FiddleComponent code='<Badge pill variant="success">
+                    Success
+                </Badge>' scope={scope} ></FiddleComponent>
                 ahmet emre
+
             </div>
         );
     }
