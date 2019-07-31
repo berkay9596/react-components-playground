dbbf54e4d8ab8e6f68be05eadbbacac43ad7f6d8
ahmetemrekilinc <aekilinc@gmail.com>
2019-07-31 17:06:12 +0300

    39

--- src/components/Addon39.js
+++ src/components/Addon39.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 
-class Addon17 extends Component {
+class Addon39 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon39</div>" ></FiddleComponent>
                 ahmet emre
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon39;
