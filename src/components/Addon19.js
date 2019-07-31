65fe9467e725dd3dc98513f19a37340e8a4b9aa7
tanfurkan <tanfurkan@gmail.com>
2019-07-31 17:11:28 +0300

    Furkan TAN

--- src/components/Addon19.js
+++ src/components/Addon19.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
-class Addon17 extends Component {
+const scope = {ProgressBar}
+
+class Addon19 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code='
+                    <div>
+                        <ProgressBar variant="success" now={40} />
+                        <ProgressBar variant="info" now={20} />
+                        <ProgressBar variant="warning" now={60} />
+                        <ProgressBar variant="danger" now={80} />
+                    </div>' scope={scope} >
+                </FiddleComponent>
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon19;
