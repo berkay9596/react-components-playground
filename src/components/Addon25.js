e0c63a8a31e89c197040599f7a496b9af7745bb2
fatihsaracoglu <fatihsaracoglu@outlook.com>
2019-07-31 17:11:53 +0300

    Edited Addon25.js

--- src/components/Addon25.js
+++ src/components/Addon25.js
@@ -5,7 +5,7 @@ class Addon25 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon25</div>" ></FiddleComponent>
                 Fatih Saracoglu
             </div>
         );
