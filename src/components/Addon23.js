d6ead59b6a725755f12023b4c7b00cd87e69102e
SelvaAydinn <selvaaydin972@gmail.com>
2019-07-31 17:11:05 +0300

    23

--- src/components/Addon23.js
+++ src/components/Addon23.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 
-class Addon17 extends Component {
+class Addon23 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon23</div>" ></FiddleComponent>
+               SELVA A
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon23;
