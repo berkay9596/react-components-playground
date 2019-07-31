5d990bfb58f38db905345a6c7ce48c640ca35e89
zego0 <zeynep9698@hotmail.com>
2019-07-31 17:05:55 +0300

    isim eklendi

--- src/components/Addon18.js
+++ src/components/Addon18.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 
-class Addon17 extends Component {
+class Addon18 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon18</div>" ></FiddleComponent>
+                Zeynep Gözel
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon18;
