210f48414f1bd25ed972dcd2cb91654f518a8ee0
EGITIM <reyyansarikaya@gmail.com>
2019-07-31 17:08:09 +0300

    24

--- src/components/Addon24.js
+++ src/components/Addon24.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 
-class Addon17 extends Component {
+class Addon24 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon24</div>" ></FiddleComponent>
+           reyyan sarıkaya
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon24;
