02e7b968c14e47e0dc7a6f81bf6a9c383beba6b6
zego0 <zeynep9698@hotmail.com>
2019-07-31 17:27:06 +0300

    isim eklendi

--- src/components/Addon18.js
+++ src/components/Addon18.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import FiddleComponent from "./FiddleComponent";
 
+import ReactDOM from 'react-dom';
+
+import FiddleComponent from "./FiddleComponent";
+import Spinner from 'react-bootstrap/Spinner';
+const scope = {Spinner}
 class Addon18 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon18</div>" ></FiddleComponent>
+                <FiddleComponent code='<div>
+                <Spinner animation="border" variant="primary" />
+                                         <Spinner animation="border" variant="secondary" />
+                                         <Spinner animation="border" variant="success" />
+                                         <Spinner animation="border" variant="danger" />
+                                         <Spinner animation="border" variant="warning" />
+                                         <Spinner animation="border" variant="info" />
+                                         <Spinner animation="border" variant="light" />
+                                         <Spinner animation="border" variant="dark" />
+                                         <Spinner animation="grow" variant="primary" />
+                                         <Spinner animation="grow" variant="secondary" />
+                                         <Spinner animation="grow" variant="success" />
+                                         <Spinner animation="grow" variant="danger" />
+                                         <Spinner animation="grow" variant="warning" />
+                                         <Spinner animation="grow" variant="info" />
+                                         <Spinner animation="grow" variant="light" />
+                                         <Spinner animation="grow" variant="dark" />
+                                         </div>' scope= {scope} ></FiddleComponent>
                 Zeynep Gözel
             </div>
         );
