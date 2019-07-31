6701b13ea78c06fd0590de4f320e6c3fc91a4210
fatihsaracoglu <fatihsaracoglu@outlook.com>
2019-07-31 18:50:28 +0300

    Added jumbotron component to Addon25.js

--- src/components/Addon25.js
+++ src/components/Addon25.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button'
+
+const scope = {Jumbotron, Button};
 
 class Addon25 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon25</div>" ></FiddleComponent>
+                <FiddleComponent code='<div>
+                <Jumbotron>
+                    <h1>Hello, world!</h1>
+                    <p>This is a simple hero unit, a simple jumbotron-style component for calling
+                       extra attention to featured content or information.</p>
+                    <p>
+                        <Button variant="primary" href="#/learnmore">Learn more</Button>
+                    </p>
+                 </Jumbotron></div>'scope = {scope}></FiddleComponent>
+
                 Fatih Saracoglu
             </div>
         );
