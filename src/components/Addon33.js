876a87a59b878cb6adda675897c2ce0baf5d911a
ivibrahim <ivibrahim@hotmail.com>
2019-07-31 23:30:46 +0300

    commm

--- src/components/Addon33.js
+++ src/components/Addon33.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Alert from 'react-bootstrap/Alert';
 
+const scope = {Alert};
 class Addon33 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
-                ibra
+                <FiddleComponent code= "<Alert variant='primary'> This is addon 33. Check it out! </Alert>" scope={scope}>
+                </FiddleComponent>
             </div>
         );
     }
