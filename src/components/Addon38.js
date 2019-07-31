4a37068dce772e1258060daaedb73a9b6a94c389
berkay9596 <53509972+berkay9596@users.noreply.github.com>
2019-07-31 17:17:37 +0300

    Update Addon38.js

--- src/components/Addon38.js
+++ src/components/Addon38.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 
-class Addon17 extends Component {
+class Addon38 extends Component {
     render() {
         return (
-            <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+  <ButtonToolbar>
+  <Button variant="outline-primary">Primary</Button>
+  <Button variant="outline-secondary">Secondary</Button>
+  <Button variant="outline-success">Success</Button>
+  <Button variant="outline-warning">Warning</Button>
+            </ButtonToolbar>
+                <FiddleComponent code="<div>this is Addon38</div>" ></FiddleComponent>
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon38;
