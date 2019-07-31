493d5df2735cbcef105e2289b3fa05d27ac37794
erdemmucahitt <mchterdm@gmail.com>
2019-07-31 17:11:32 +0300

    mücahit  erdem

--- src/components/Addon15.js
+++ src/components/Addon15.js
@@ -5,7 +5,9 @@ class Addon15 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon15</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon15 Mücahit Erdem</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon15 Mücahit Erdem</div>" ></FiddleComponent>
+
             </div>
         );
     }
