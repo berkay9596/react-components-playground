17b375e58899bb6fef483598658f40e90ab64179
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2019-07-31 20:30:34 +0300

    added progress bar component.

--- src/components/Addon17.js
+++ src/components/Addon17.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import progress from 'react-bootstrap/ProgressBar';
 
+
+const scope = {progress};
 class Addon17 extends Component {
     render() {
         return (
+
             <div>
-                <FiddleComponent code="<div>Furkan Caglayan</div>" ></FiddleComponent>
+                <FiddleComponent code='<div className="progress">
+  <div className="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>' scope={scope} ></FiddleComponent>
+
+                <FiddleComponent code='<div className="progress">
+  <div className="progress-bar bg-warning w-25" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>' scope={scope} ></FiddleComponent>
+       
+
+
             </div>
         );
     }
