73198710d49dc4af6ebf40717fb57902682dba35
Den-100 <nurdeniz100@hotmail.com>
2019-08-01 09:18:29 +0300

    homework

--- src/components/Addon22.js
+++ src/components/Addon22.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 
-class Addon17 extends Component {
+class Addon22 extends Component {
     render() {
         return (
             <div>
                 <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
+                nur deniz cayli
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon22;
