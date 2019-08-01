7d2c36d2d25842d4f1270d6832e6bac47bad8784
onurtgmn <onursimsek0643@gmail.com>
2019-08-01 11:59:04 +0300

    Addon20

--- src/components/Addon20.js
+++ src/components/Addon20.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 import Button from 'react-bootstrap/Button' ;
 
+const scope = {Button}
+
 class Addon20 extends Component {
     render() {
         return (
             <div>
 
-                <FiddleComponent code="<div>this is Addon20</div>" ></FiddleComponent>
-				<Button variant="warning">Warning</Button>
-				onur simsek
+                <FiddleComponent code='<Button pill variant="warning">
+                    Warning
+                    </Button>' scope = {scope}>
+
+
+
+                </FiddleComponent>
+
+				Cretaed by onur simsek
             </div>
         );
     }
