7391e3182e8abe9f75a6ac36bedf4a31696f6b2a
acabak98 <anil.cabak@metu.edu.tr>
2019-08-01 15:46:11 +0300

    outline buttons

--- src/components/Addon31.js
+++ src/components/Addon31.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Button from 'react-bootstrap/Button';
+
+
+const scope = {Button};
 
 class Addon31 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon31</div>" ></FiddleComponent>
-                anıl cabak
+                <FiddleComponent code="<div> this is Addon31
+                    <br/>
+                   <Button variant='outline-warning'>Warning</Button>
+                </div>" scope={scope} ></FiddleComponent>
             </div>
         );
     }
