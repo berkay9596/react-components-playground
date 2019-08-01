fe879ff7360496c0cee3cb235cb96d733c5032a7
Meltem Tokgöz <meltemtgz@gmail.com>
2019-08-01 09:18:28 +0300

    Update Addon37.js

--- src/components/Addon37.js
+++ src/components/Addon37.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
 import Badge from 'react-bootstrap/Badge';
 
+const scope = {Badge};
+
 class Addon37 extends Component {
     render() {
         return (
             <div>
                 <FiddleComponent code='<h1>
                 Example heading <Badge variant="secondary">New</Badge>
-              </h1>' scope={scope} ></FiddleComponent>
-                meltem 
+                </h1>' scope={scope} ></FiddleComponent>
+                Meltem Tokgöz
             </div>
         );
     }
