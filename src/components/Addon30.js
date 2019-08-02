46739c6c55ea1954fd12605252ee657a1efd6934
Faruk BALCI <farukbalci99@hotmail.com>
2019-08-02 16:10:28 +0300

    Faruk Balcı odev1 addon30

--- src/components/Addon30.js
+++ src/components/Addon30.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Card from 'react-bootstrap/Card';
 
-class Addon17 extends Component {
+const scope = {Card};
+class Addon30 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>Faruk Balcı</div>" ></FiddleComponent>
+                <FiddleComponent code='<Card style={{ width: "18rem" }}>
+                                         <Card.Body>
+                                           <Card.Title>Card Title</Card.Title>
+                                           <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                           <Card.Text>
+                                             Some quick example text
+                                           </Card.Text>
+                                           <Card.Link href="#">Card Link</Card.Link>
+                                           <Card.Link href="#">Another Link</Card.Link>
+                                         </Card.Body>
+                                       </Card>' scope={scope} ></FiddleComponent>
+                                       Faruk Balcı
             </div>
         );
     }
 }
 
-export default Addon17;
+export default Addon30;
