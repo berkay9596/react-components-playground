bea03df163ba7a7a8ad341d2303cdc667551e3c8
Koray Kara <koray.kara98.kk@gmail.com>
2019-08-03 20:57:22 +0300

    New Commit

--- src/components/Addon28.js
+++ src/components/Addon28.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
-
+import ButtonToolbar  from 'react-bootstrap/ButtonToolbar';
+import Button  from 'react-bootstrap/Button';
+const scope = { ButtonToolbar ,Button };
 class Addon17 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon17</div>" ></FiddleComponent>
-                Koray Kara
+            <FiddleComponent code="
+            <div>
+            <ButtonToolbar>
+            <Button >
+              Large button
+            </Button>
+            <Button>
+              Large button
+            </Button>
+          </ButtonToolbar>
+            
+          <ButtonToolbar>
+            <Button>
+              Small button
+            </Button>
+            <Button>
+              Small button
+            </Button>
+          </ButtonToolbar> 
+            <h1>Koray Kara</h1>
+            </div>
+            " scope={scope} ></FiddleComponent>
+            
             </div>
         );
     }
