a1304b74a344aa37c604f4bd2d40fa10fe0f0fef
YEKTA <yektasokelen@gmail.com>
2019-08-05 16:37:47 +0300

    Harun Yekta Sökelen

--- src/components/Addon40.js
+++ src/components/Addon40.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Alert from "react-bootstrap/Alert";
+import Badge from "react-bootstrap/Badge";
 
+const scope = {Alert};
 class Addon40 extends Component {
     render() {
         return (
+
             <div>
-                <FiddleComponent code="<div>this is Addon40</div>" ></FiddleComponent>
+                <FiddleComponent code='<Alert variant="success">
+  <Alert.Heading>Hey, nice to see you</Alert.Heading>
+  <p>
+    React Odev1
+  </p>
+  <hr />
+  <p className="mb-0">
+   Harun Yekta Sökelen
+  </p>
+</Alert>' scope = {scope}></FiddleComponent>
+
             </div>
         );
     }
