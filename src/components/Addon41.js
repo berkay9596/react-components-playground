f74f072f67b2dabf2f20853350a5165f636fdb64
Ahmet Ozel <ahmt.ozel@hotmail.com>
2019-08-05 16:37:45 +0300

    Ahmet Ozel addon

--- src/components/Addon41.js
+++ src/components/Addon41.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
+const scope = {Card, Button};
 class Addon41 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon41</div>" ></FiddleComponent>
+                <FiddleComponent code="<div>this is Addon41
+                <Card>
+                  <Card.Header>Card</Card.Header>
+                  <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                      With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                    <Button variant='primary'>Go nowhere</Button>
+                  </Card.Body>
+                </Card>
+                </div>" scope={scope}></FiddleComponent>
+
             </div>
         );
     }
