cb7d8c7188205f2d5bbb03e61f528c8a9d81929c
HulyaKutay <HulyaKutay@users.noreply.github.com>
2019-08-06 17:03:47 +0300

    Hülya KUTAY

--- src/components/Addon42.js
+++ src/components/Addon42.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
-
+import Button from "react-bootstrap/Button";
 class Addon42 extends Component {
     render() {
         return (
             <div>
                 <FiddleComponent code="<div>this is Addon42</div>" ></FiddleComponent>
+            <button className="btn">Left</button>
+            <button className="btn">Middle</button>
+            <button className="btn">Right</button>
             </div>
         );
     }
