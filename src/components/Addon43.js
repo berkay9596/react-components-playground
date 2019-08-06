c7c7a99f270f416a6622e40dd65ca55f11fffd71
hulyabasaran <hb.973148@gmail.com>
2019-08-06 16:51:02 +0300

    Hülya Başaran

--- src/components/Addon43.js
+++ src/components/Addon43.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
-
+import Button from "react-bootstrap/Button";
 class Addon43 extends Component {
     render() {
         return (
             <div>
                 <FiddleComponent code="<div>this is Addon43</div>" ></FiddleComponent>
-            gddg
+            <button className="btn">Left</button>
+            <button className="btn">Middle</button>
+            <button className="btn">Right</button>
             </div>
         );
     }
